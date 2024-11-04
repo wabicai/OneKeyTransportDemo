9e749f40d765bd8be6949f985809b71c09b78e26
蔡凯升 <kaisheng.cai@onekey.so>
2024-11-04 20:42:09 +0800

    fix: delete unused code

--- OneKeyBleDemo/OKBleDefines.m
+++
@@ -1,9 +0,0 @@
-#import "OKBleDefines.h"
-
-NSString *const OKBleErrorDomain = @"com.onekey.ble";
-
-const NSTimeInterval kBleOperationTimeout = 30.0;
-
-NSString *const kServiceUUID = @"00000001-0000-1000-8000-00805f9b34fb";
-NSString *const kWriteUUID = @"00000002-0000-1000-8000-00805f9b34fb";
-NSString *const kNotifyUUID = @"00000003-0000-1000-8000-00805f9b34fb"; 
